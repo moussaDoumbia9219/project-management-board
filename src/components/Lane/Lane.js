03d0c7af43c4a19ade5ca47021968a505540de58
moussa Doumbia <moussa.doumbia9219@gmail.com>
2020-03-19 18:25:03 +0100

    implement drag and drop functionnality

--- src/components/Lane/Lane.js
+++ src/components/Lane/Lane.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import Ticket from '../Ticket/Ticket';
+import React from "react";
+import styled from "styled-components";
+import Ticket from "../Ticket/Ticket";
 
 const TicketsWrapper = styled.div`
   padding: 5%;
@@ -31,12 +31,23 @@ const Title = styled.h2`
   border-bottom: 1px solid darkGray;
 `;
 
-const Lane = ({ tickets, loading, error, title }) => (
-  <LaneWrapper>
+const Lane = ({
+  laneId,
+  tickets,
+  loading,
+  error,
+  onDragStart,
+  onDragOver,
+  onDrop,
+  title
+}) => (
+  <LaneWrapper onDragOver={e => onDragOver(e)} onDrop={e => onDrop(e, laneId)}>
     <Title>{title}</Title>
-    {(loading || error) && <Alert>{loading ? 'Loading...' : error}</Alert>}
+    {(loading || error) && <Alert>{loading ? "Loading..." : error}</Alert>}
     <TicketsWrapper>
-    {tickets.map(ticket => <Ticket key={ticket.id} ticket={ticket} />)}
+      {tickets.map(ticket => (
+        <Ticket key={ticket.id} onDragStart={onDragStart} ticket={ticket} />
+      ))}
     </TicketsWrapper>
   </LaneWrapper>
 );
