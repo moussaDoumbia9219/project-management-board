bdc1c49968c339b91762a5ee4a8da01c344660fa
moussa Doumbia <moussa.doumbia9219@gmail.com>
2020-03-19 17:15:13 +0100

    implement HOC logique

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import Lane from '../components/Lane/Lane';
+import React from "react";
+import styled from "styled-components";
+import withDataFetching from "../WithDataFetching";
+import Lane from "../components/Lane/Lane";
 
 const BoardWrapper = styled.div`
   display: flex;
@@ -13,54 +14,18 @@ const BoardWrapper = styled.div`
   }
 `;
 
+const Board = ({ lanes, loading, error, data }) => (
+  <BoardWrapper>
+    {lanes.map(lane => 
+      <Lane
+        key={lane.id}
+        title={lane.title}
+        loading={loading}
+        error={error}
+        tickets={data.filter(ticket => ticket.lane === lane.id)}
+      />
+    )}
+  </BoardWrapper>
+);
 
-class Board extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: [],
-      loading: true,
-      error: '',
-    }
-  }
-
-  async componentDidMount() {
-    try {
-      const tickets = await fetch('../../assets/data.json');
-      const ticketJSON = await tickets.json();
-
-      if(ticketJSON) {
-        this.setState({
-          data: ticketJSON,
-          loading: false
-        });
-      }
-    } catch(error) {
-      this.setState({
-        loading:false,
-        error: error.message
-      })
-    }
-  }
-
-  render() {
-    const {data, loading, error} = this.state;
-
-    const lanes = [
-      { id: 1, title: 'To Do' },
-      { id: 2, title: 'In Progress' },
-      { id: 3, title: 'Review' },
-      { id: 4, title: 'Done' },
-    ];
-
-    return (
-      <BoardWrapper>
-        {lanes.map(lane => (
-          <Lane key={lane.id} title={lane.title} loading={loading} error={error} tickets={data.filter(ticket => ticket.lane === lane.id)} />
-        ))}
-      </BoardWrapper>
-    );
-  }
-}
-
-export default Board;
+export default withDataFetching(Board);
